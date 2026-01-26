9842ad920b74a81522ea646e54d7fbf03a1d3638
Athitya V <athityav22@gmail.com>
2026-01-26 17:35:37 +0530

    Add files via upload

---
+++ SeleniumLogin.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumLogin {
+
+	public static void main(String[] args) {
+		System.setProperty("Webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.youtube.com/");
+		
+
+	}
+
+}
